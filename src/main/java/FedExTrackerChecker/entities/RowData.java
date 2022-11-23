bd681ee3ac1a7725d1602feaba00cd6ffb492aca
uixneg0 <uixneg0@gmail.com>
2022-11-22 22:23:36 -0600

    populate rowdata

--- src/main/java/FedExTrackerChecker/entities/RowData.java
+++ src/main/java/FedExTrackerChecker/entities/RowData.java
@@ -8,6 +8,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class RowData {
-    private final long trackingNumber;
-    //todo populate with the rest
+    private final String trackingNumber;
+    private final String packageWeight;
+    private final String sequenceNumber;
+    private final String shipperCity;
+    private final String actualDeliveryAddressCity;
+    private final String deliveryAttempts;
+    private final String deliverySignature;
+    private final String eventDescription;
+    private final String reference1;
+    private final String reference2;
+    private final String reference3;
 }
