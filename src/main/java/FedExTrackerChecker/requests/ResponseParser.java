9eb1751c3f71b0dfc96147f0c8f0cff6165e9576
uixneg0 <uixneg0@gmail.com>
2022-11-22 22:39:54 -0600

    Update ResponseParser.java

--- src/main/java/FedExTrackerChecker/requests/ResponseParser.java
+++ src/main/java/FedExTrackerChecker/requests/ResponseParser.java
@@ -1,8 +1,17 @@
 package FedExTrackerChecker.requests;
 
+import FedExTrackerChecker.entities.RowData;
 import okhttp3.Response;
 
+import java.util.ArrayList;
+
 public class ResponseParser {
+
+    public static ArrayList<RowData> parseResponses() {
+        ArrayList<RowData> rowDataList = new ArrayList<>();
+        return rowDataList;
+    }
+
     public static String getKeyValue(Response response, String key) {
         return "";
     }
